9aec6fee2dc9cdf68a4c5a27abe324965ad923af
ONJORO <eonjoro35@gmail.com>
2026-09-22 21:08:41 +0300

    Fix Pages Vite config override

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/ST.Billy/",
+    base: "/st_billy_school/",
     plugins: [react()],
     resolve: {
         alias: {
